11db6c8d18385f1ad3c92d7a1e593137dda65e04
PrashantPatil10178 <patilanilprashant178@gmail.com>
2025-11-01 18:16:26 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {
+  const MyContract = await hre.ethers.getContractFactory("MyContract");
+  const myContract = await MyContract.deploy("Hello, Hardhat!");
+
+  await myContract.waitForDeployment();
+  console.log(`Contract deployed to: ${await myContract.getAddress()}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
