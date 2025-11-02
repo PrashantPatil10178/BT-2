a6f83d470d819a860fa3013af1a7e78f4b05d392
PrashantPatil10178 <patilanilprashant178@gmail.com>
2025-11-02 13:37:11 +0530

    Updated code

--- scripts/test-contract.js
+++ scripts/test-contract.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 async function main() {
   // The deployed contract address
-  const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+  const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
   // Get the contract factory and attach to deployed instance
   const WavePortal = await hre.ethers.getContractFactory("WavePortal");
